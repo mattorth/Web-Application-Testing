b5bcfcfe36d7c244ed70bd6640e052b8e6ffcb8c
mattorth <orth.mj@gmail.com>
2019-11-09 18:18:56 -0800

    mvp

--- testing/src/App.test.js
+++ testing/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import * as rtl from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders without crashing', () => {
+  render(<App />);
 });
+
+test('it displays the text Strikes: 0', () => {
+  const container = render(<App />);
+  container.getByText('Strikes: 0');
+})
+
+test('it displays the text Balls: 0', () => {
+  const { getByText } = render(<App />);
+  getByText('Balls: 0');
+})
